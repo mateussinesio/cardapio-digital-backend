9febfabe3a0bd7b7b0e741adc15c9b906810732d
Mateus Sinésio <mateussinesio@gmail.com>
2024-12-24 17:04:21 -0300

    now the images of the categories are saved locally and the URL of the images are saved in the database

--- src/main/java/menu/dtos/CategoryResponseDTO.java
+++ src/main/java/menu/dtos/CategoryResponseDTO.java
@@ -1,10 +1,11 @@
 package menu.dtos;
 
 import menu.model.Category;
-import java.util.Base64;
+
+import java.nio.file.Paths;
 
 public record CategoryResponseDTO(String id, String name, String image) {
     public CategoryResponseDTO(Category category) {
-        this(category.getId(), category.getName(), Base64.getEncoder().encodeToString(category.getImage()));
+        this(category.getId(), category.getName(), "/images/" + Paths.get(category.getImagePath()).getFileName().toString());
     }
 }
